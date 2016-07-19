3dee4d036a374e80323dbf5d9dff742d3d47b43e
dinukanadeeshan <dinuka.nadeeshan@yahoo.com>
2016-07-19 10:14:06 +0530

    Add new labs for co224

--- CO225_Software Construction - LAB/Lab5/Lab5/src/edu/co225/lab5/Main.java
+++ CO225_Software Construction - LAB/Lab5/Lab5/src/edu/co225/lab5/Main.java
@@ -6,50 +6,58 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Array ar = new Array();
-
-        ar.add(10);
-        ar.add(11);
-        ar.add(12);
-        ar.add(13);
-        ar.add(14);
-        ar.add(15);
-        ar.add(16);
-        ar.add(17);
-        ar.add(18);
-        ar.add(19);
-        ar.add(20);
-        ar.add(21);
-        ar.add(22);
-
-        System.out.println(ar);
-
-
-        System.out.println(ar.get(5));
-
-        ar.remove(5);
-
-        System.out.println(ar);
-
-        ar.add(5,100);
-
-        System.out.println(ar);
-
-        ar.replace(6, 150);
-        //ar.replace(120, 150);
-
-        System.out.println(ar);
-
-        ar.clear();
-        System.out.println(ar);
-
-        ar.add(10);
-        ar.add(11);
-        ar.add(12);
-        ar.add(13);
-        ar.add(14);
-
-        System.out.println(ar);
+//        Array ar = new Array();
+//
+//        ar.add(10);
+//        ar.add(11);
+//        ar.add(12);
+//        ar.add(13);
+//        ar.add(14);
+//        ar.add(15);
+//        ar.add(16);
+//        ar.add(17);
+//        ar.add(18);
+//        ar.add(19);
+//        ar.add(20);
+//        ar.add(21);
+//        ar.add(22);
+//
+//        System.out.println(ar);
+//
+//
+//        System.out.println(ar.get(5));
+//
+//        ar.remove(5);
+//
+//        System.out.println(ar);
+//
+//        ar.add(5,100);
+//
+//        System.out.println(ar);
+//
+//        ar.replace(6, 150);
+//        //ar.replace(120, 150);
+//
+//        System.out.println(ar);
+//
+//        ar.clear();
+//        System.out.println(ar);
+//
+//        ar.add(10);
+//        ar.add(11);
+//        ar.add(12);
+//        ar.add(13);
+//        ar.add(14);
+//
+//        System.out.println(ar);
+//
+//
+        try {
+            return;
+        }finally {
+            System.out.println("Hello");
+        }
+        
 
     }
 }
